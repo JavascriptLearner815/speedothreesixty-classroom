fd8aaea8dd6459f540be02be8e09238351b3101b
Dean Summer <deanlovesmargie@gmail.com>
2020-12-10 11:05:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-let redirectUrl = document.getElementById("redirect").value;
-let redirectButton = document.getElementById("redirect-go");
-redirectButton.addEventListener("click", () => {
-  if (redirectUrl) window.open(redirectUrl);
-});
+
