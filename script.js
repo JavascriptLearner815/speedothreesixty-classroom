ec1fcc30dfc9b58ef84ad89fe5645d7941b3eac2
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-04-24 17:48:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Base classroom website URL
+const BASE_URL = "https://javascriptlearner815.github.io/speedothreesixty-classroom/"
+
+// Array of footer links
+const FOOTER_LINKS = [
+  ["https://classroom.google.com/c/MjAxNjU0ODAyNjY2?cjc=visrdmf", "Join our classroom!"],
+  ["https://stackoverflow.com/users/13900902/speedothreesixty", "View information about/contact information about the developer, SpeedoThreeSixty!"],
+  ["https://github.com/JavascriptLearner815/speedothreesixty-classroom", "View this resource page's code on GitHub!"],
+]
+
+// Number of footer links so far
+let footerLinkCount = 0
+
 // Create a class for the backpack element
 class Backpack extends HTMLElement {
   constructor() {
@@ -49,3 +62,39 @@ class Backpack extends HTMLElement {
 
 // Define the new element
 customElements.define("backpack", Backpack)
+
+// Create a class for header links
+class HeaderLink {
+  constructor(afterBase, name) {
+    const newElement = document.createElement("a")
+    newElement.href = `${BASE_URL}${afterBase}`
+    newElement.innerText = name
+    this.elem = newElement
+  }
+}
+
+// Add header links
+const links = document.querySelector("header nav")
+links.appendChild(new HeaderLink("", "Home").elem)
+links.appendChild(new HeaderLink("language/", "Language Lessons").elem)
+links.appendChild(new HeaderLink("audio/", "Audio Recordings").elem)
+links.appendChild(new HeaderLink("copyright/", "Copyright").elem)
+links.appendChild(new HeaderLink("tests/", "Tests").elem)
+links.appendChild(new HeaderLink("games/", "Games").elem)
+
+// Create a class for footer links
+class FooterLink {
+  constructor(index) {
+    const newContainerElement = document.createElement("p")
+    let link = FOOTER_LINKS[index][0]
+    let text = FOOTER_LINKS[index][1]
+    newContainerElement.innerHTML = `<a id="footerlink${++footerLinkCount}" href="${link}" target="_blank">${text}</a>`
+    this.elem = newContainerElement
+  }
+}
+
+// Add footer links
+const links2 = document.querySelector("footer nav")
+links2.appendChild(new FooterLink(0).elem)
+links2.appendChild(new FooterLink(1).elem)
+links2.appendChild(new FooterLink(2).elem)
