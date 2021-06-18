70d2e9e6920ad23f97df5ea608a04d2b75830f06
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-06-18 12:00:57 -0500

    Update webcam.js

--- tests/webcam.js
+++ tests/webcam.js
@@ -1,5 +1,21 @@
 const webcam = document.getElementById("webcam")
 
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {}
+}
+
+if (navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = function(constraints) {
+    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.oGetUserMedia || navigator.msGetUserMedia || navigator.getUserMedia
+    if (!getUserMedia) {
+      return Promise.reject(new Error("getUserMedia is not implemented in this browser"))
+    }
+    return new Promise(function(resolve, reject) {
+      getUserMedia.call(navigator, constraints, resolve, reject)
+    }
+  }
+}
+
 navigator.mediaDevices.getUserMedia(
   {
     audio: false,
@@ -14,13 +30,29 @@ navigator.mediaDevices.getUserMedia(
         ideal: 650,
         max: 2098
       },
+      frameRate: {
+        min: 1,
+        ideal: 60,
+        max: 180
+      },
       facingMode: "user"
     }
   }
 )
-  .then(stream => {
-    webcam.srcObject = stream
+  .then(function(stream) {
+    addStream(stream)
+    webcam.onloadedmetadata = function(e) {
+      video.play()
+    }
   })
-  .catch(err => {
-    console.error(err)
+  .catch(function(err) {
+    console.error(`${err.name}: ${err.message}`)
   })
+
+  
+function addStream(stream) {
+  if ("srcObject" in webcam) {
+    return webcam.srcObject = stream
+  }
+  webcam.src = window.URL.createObjectURL(stream)
+}
